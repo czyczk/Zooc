998e0efdffafbc1415094fe4977739680945193d
Zenas Chen <czyczk@qq.com>
2018-06-22 21:44:45 +0800

    Table construction SQL statements

---
+++ sql/zooc.sql
@@ -0,0 +1,264 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : Lab
+Source Server Version : 80011
+Source Host           : localhost:3306
+Source Database       : zooc
+
+Target Server Type    : MYSQL
+Target Server Version : 80011
+File Encoding         : 65001
+
+Date: 2018-06-22 21:43:43
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for administrator
+-- ----------------------------
+DROP TABLE IF EXISTS `administrator`;
+CREATE TABLE `administrator` (
+  `administrator_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(16) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`administrator_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for branch
+-- ----------------------------
+DROP TABLE IF EXISTS `branch`;
+CREATE TABLE `branch` (
+  `branch_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `enterprise_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `address` varchar(127) COLLATE utf8mb4_general_ci NOT NULL,
+  `latitude` decimal(11,8) NOT NULL,
+  `longitude` decimal(11,8) NOT NULL,
+  `telephone` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`branch_id`),
+  KEY `fk_branch_enterprise_id` (`enterprise_id`),
+  CONSTRAINT `fk_branch_enterprise_id` FOREIGN KEY (`enterprise_id`) REFERENCES `enterprise` (`enterprise_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course` (
+  `course_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(63) COLLATE utf8mb4_general_ci NOT NULL,
+  `detail` text COLLATE utf8mb4_general_ci NOT NULL,
+  `img_url` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
+  `category_id` bigint(20) unsigned NOT NULL,
+  `release_time` datetime NOT NULL,
+  `price` decimal(10,2) unsigned NOT NULL,
+  `status` enum('AVAILABLE','OFF','IN_REVIEW') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'IN_REVIEW',
+  PRIMARY KEY (`course_id`),
+  KEY `fk_course_category_id` (`category_id`),
+  CONSTRAINT `fk_course_category_id` FOREIGN KEY (`category_id`) REFERENCES `course_category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for course_category
+-- ----------------------------
+DROP TABLE IF EXISTS `course_category`;
+CREATE TABLE `course_category` (
+  `category_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for course_offering
+-- ----------------------------
+DROP TABLE IF EXISTS `course_offering`;
+CREATE TABLE `course_offering` (
+  `course_offering_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `course_id` bigint(20) unsigned NOT NULL,
+  `branch_id` bigint(20) unsigned NOT NULL,
+  `lecturer_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`course_offering_id`),
+  KEY `fk_course_offering` (`course_id`),
+  KEY `fk_course_offering_branch_id` (`branch_id`),
+  KEY `fk_course_offering_lecturer_id` (`lecturer_id`),
+  CONSTRAINT `fk_course_offering` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_course_offering_branch_id` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`branch_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_course_offering_lecturer_id` FOREIGN KEY (`lecturer_id`) REFERENCES `lecturer` (`lecturer_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for enterprise
+-- ----------------------------
+DROP TABLE IF EXISTS `enterprise`;
+CREATE TABLE `enterprise` (
+  `enterprise_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `img` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
+  `introduction` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
+  `video_url` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
+  `detail` text COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`enterprise_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for lecturer
+-- ----------------------------
+DROP TABLE IF EXISTS `lecturer`;
+CREATE TABLE `lecturer` (
+  `lecturer_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `photo_url` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
+  `introduction` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
+  `branch_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`lecturer_id`),
+  KEY `fk_lecturer_branch_id` (`branch_id`),
+  CONSTRAINT `fk_lecturer_branch_id` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`branch_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for moment
+-- ----------------------------
+DROP TABLE IF EXISTS `moment`;
+CREATE TABLE `moment` (
+  `moment_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `enterprise_id` bigint(20) unsigned NOT NULL,
+  `content` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`moment_id`),
+  KEY `fk_moment_enterprise_id` (`enterprise_id`),
+  CONSTRAINT `fk_moment_enterprise_id` FOREIGN KEY (`enterprise_id`) REFERENCES `enterprise` (`enterprise_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for moment_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `moment_comment`;
+CREATE TABLE `moment_comment` (
+  `moment_comment_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `moment_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `content` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`moment_comment_id`),
+  KEY `fk_moment_comment_moment_id` (`moment_id`),
+  KEY `fk_moment_comment_user_id` (`user_id`),
+  CONSTRAINT `fk_moment_comment_moment_id` FOREIGN KEY (`moment_id`) REFERENCES `moment` (`moment_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_moment_comment_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for moment_img
+-- ----------------------------
+DROP TABLE IF EXISTS `moment_img`;
+CREATE TABLE `moment_img` (
+  `moment_img_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `moment_id` bigint(20) unsigned NOT NULL,
+  `img_url` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`moment_img_id`),
+  KEY `fk_moment_img_moment_id` (`moment_id`),
+  CONSTRAINT `fk_moment_img_moment_id` FOREIGN KEY (`moment_id`) REFERENCES `moment` (`moment_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for moment_like
+-- ----------------------------
+DROP TABLE IF EXISTS `moment_like`;
+CREATE TABLE `moment_like` (
+  `moment_like_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `moment_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`moment_like_id`),
+  KEY `fk_moment_like_moment_id` (`moment_id`),
+  KEY `fk_moment_like_user_id` (`user_id`),
+  CONSTRAINT `fk_moment_like_moment_id` FOREIGN KEY (`moment_id`) REFERENCES `moment` (`moment_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_moment_like_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for order
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `order_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `course_id` bigint(20) unsigned NOT NULL,
+  `time` datetime NOT NULL,
+  `status` enum('PENDING','CANCELED','AVAILABLE','REFUND_REQUESTED','REFUNDED') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'PENDING',
+  PRIMARY KEY (`order_id`),
+  KEY `fk_order_user_id` (`user_id`),
+  KEY `fk_order_course_id` (`course_id`),
+  CONSTRAINT `fk_order_course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_order_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for refund
+-- ----------------------------
+DROP TABLE IF EXISTS `refund`;
+CREATE TABLE `refund` (
+  `refund_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(20) unsigned NOT NULL,
+  `time` datetime NOT NULL,
+  `reason` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`refund_id`),
+  KEY `fk_refund_order_id` (`order_id`),
+  CONSTRAINT `fk_refund_order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for trial
+-- ----------------------------
+DROP TABLE IF EXISTS `trial`;
+CREATE TABLE `trial` (
+  `trial_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(63) COLLATE utf8mb4_general_ci NOT NULL,
+  `detail` text COLLATE utf8mb4_general_ci NOT NULL,
+  `img_url` varchar(511) COLLATE utf8mb4_general_ci NOT NULL,
+  `branch_id` bigint(20) unsigned NOT NULL,
+  `lecturer_id` bigint(20) unsigned NOT NULL,
+  `release_time` datetime NOT NULL,
+  `status` enum('AVAILABLE','OFF','IN_REVIEW') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'IN_REVIEW',
+  PRIMARY KEY (`trial_id`),
+  KEY `fk_trial_branch_id` (`branch_id`),
+  KEY `fk_trial_lecturer_id` (`lecturer_id`),
+  CONSTRAINT `fk_trial_branch_id` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`branch_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_trial_lecturer_id` FOREIGN KEY (`lecturer_id`) REFERENCES `lecturer` (`lecturer_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for trial_reservation
+-- ----------------------------
+DROP TABLE IF EXISTS `trial_reservation`;
+CREATE TABLE `trial_reservation` (
+  `revervation_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `trial_id` bigint(20) unsigned NOT NULL,
+  `time` datetime NOT NULL,
+  `message` varchar(511) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `status` enum('PENDING','CANCELED','AVAILABLE','USED') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'PENDING',
+  PRIMARY KEY (`revervation_id`),
+  KEY `fk_trial_reservation` (`user_id`),
+  KEY `fk_trial_reservation_trial_id` (`trial_id`),
+  CONSTRAINT `fk_trial_reservation` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_trial_reservation_trial_id` FOREIGN KEY (`trial_id`) REFERENCES `trial` (`trial_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(16) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(31) COLLATE utf8mb4_general_ci NOT NULL,
+  `telephone` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `avatar_url` varchar(511) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `unique_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+SET FOREIGN_KEY_CHECKS=1;
