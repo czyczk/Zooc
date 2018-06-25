07d34c6a65fcc02a80e0fb3f2d0f299cc271870a
Zenas Chen <czyczk@qq.com>
2018-06-25 14:14:38 +0800

    Service: Comments for BranchService

--- src/main/java/com/zzzz/service/BranchService.java
+++ src/main/java/com/zzzz/service/BranchService.java
@@ -25,6 +25,16 @@ public interface BranchService {
 
     Branch getById(String branchId) throws BranchServiceException;
 
+    /**
+     * Update a branch. A field will be left unchanged if the parameter is null.
+     * @param targetBranchId Target branch ID
+     * @param name New name
+     * @param address New address
+     * @param latitude New latitude
+     * @param longitude New longitude
+     * @param telephone New telephone
+     * @throws BranchServiceException An exception is thrown if the update is not successful.
+     */
     void update(String targetBranchId,
                 String name,
                 String address,
