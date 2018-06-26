b4e3d0701d8a3087481f9aab755d612ad8207746
Zenas Chen <czyczk@qq.com>
2018-06-26 12:57:37 +0800

    DAO: CourseOffering

--- src/main/java/com/zzzz/dao/CourseOfferingDao.java
+++ src/main/java/com/zzzz/dao/CourseOfferingDao.java
@@ -1,4 +1,12 @@
 package com.zzzz.dao;
 
+import com.zzzz.po.CourseOffering;
+
+import java.sql.SQLException;
+
 public interface CourseOfferingDao {
+    int insert(CourseOffering courseOffering) throws SQLException;
+    boolean checkExistenceById(long courseOfferingId) throws SQLException;
+    CourseOffering getById(long courseOfferingId) throws SQLException;
+    int update(CourseOffering courseOffering) throws SQLException;
 }
