ff8498ef1bf45713363e2733a7dd5ff32824f05d
Zenas Chen <czyczk@qq.com>
2018-06-26 11:03:35 +0800

    Bug fix: invalid communication in LecturerController

--- src/main/java/com/zzzz/controller/LecturerController.java
+++ src/main/java/com/zzzz/controller/LecturerController.java
@@ -1,15 +1,13 @@
 package com.zzzz.controller;
 
+import com.zzzz.dto.LecturerParam;
 import com.zzzz.po.Lecturer;
 import com.zzzz.service.LecturerService;
 import com.zzzz.service.LecturerServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,19 +18,15 @@ public class LecturerController {
     /**
      * Create a new lecturer.
      * @param enterpriseId The ID of the enterprise to which the lecturer belong
-     * @param name Name
-     * @param photoUrl Photo URL
-     * @param introduction Introduction
+     * @param lecturerParam name, photoUrl, introduction
      * @return Success: Lecturer ID; Bad request: 400; Internal: 500
      */
-    @RequestMapping(value ="/course/{id}/lecturer", method = RequestMethod.POST)
+    @PostMapping(value ="/course/{id}/lecturer")
     public ResponseEntity create(@PathVariable("id") String enterpriseId,
-                                 String name,
-                                 String photoUrl,
-                                 String introduction) {
+                                 @RequestBody LecturerParam lecturerParam) {
         // TODO authentication not implemented yet
         try {
-            long lecturerId = lecturerService.insert(enterpriseId, name, photoUrl, introduction);
+            long lecturerId = lecturerService.insert(enterpriseId, lecturerParam.getName(), lecturerParam.getPhotoUrl(), lecturerParam.getIntroduction());
             return ResponseEntity.ok(lecturerId);
         } catch (LecturerServiceException e) {
             return ResponseEntity.status(e.getExceptionTypeEnum().getStatus())
@@ -45,9 +39,9 @@ public class LecturerController {
      * @param lecturerId Lecturer ID
      * @return Success: Lecturer; Bad request: 400; Not found: 404; Internal: 500
      */
-    @RequestMapping(value = "/lecturer/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/lecturer/{id}")
     // TODO authentication not implemented yet
-    public ResponseEntity getById(String lecturerId) {
+    public ResponseEntity getById(@PathVariable("id") String lecturerId) {
         try {
             Lecturer lecturer = lecturerService.getById(lecturerId);
             return ResponseEntity.ok(lecturer);
@@ -60,19 +54,15 @@ public class LecturerController {
     /**
      * Update a lecturer. Don't pass in fields meant to be kept unchanged.
      * @param targetLecturerId Target lecturer ID
-     * @param name New name
-     * @param photoUrl New photo URL
-     * @param introduction New introduction
+     * @param lecturerParam name, photoUrl, introduction
      * @return Success: 204; Bad request: 400; Internal: 500
      */
-    @RequestMapping(value = "/lecturer/{id}", method = RequestMethod.PUT)
+    @PutMapping(value = "/lecturer/{id}")
     public ResponseEntity update(@PathVariable("id") String targetLecturerId,
-                                 String name,
-                                 String photoUrl,
-                                 String introduction) {
+                                 @RequestBody LecturerParam lecturerParam) {
         // TODO authentication not implemented yet
         try {
-            lecturerService.update(targetLecturerId, name, photoUrl, introduction);
+            lecturerService.update(targetLecturerId, lecturerParam.getName(), lecturerParam.getPhotoUrl(), lecturerParam.getIntroduction());
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         } catch (LecturerServiceException e) {
             return ResponseEntity.status(e.getExceptionTypeEnum().getStatus())
---
+++ src/main/java/com/zzzz/dto/LecturerParam.java
@@ -0,0 +1,31 @@
+package com.zzzz.dto;
+
+public class LecturerParam {
+    private String name;
+    private String photoUrl;
+    private String introduction;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+}
