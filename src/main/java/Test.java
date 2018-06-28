6543fa46888b9c47fc19aa9492178895c7b77f9e
Zenas Chen <czyczk@qq.com>
2018-06-28 21:59:00 +0800

    Added and configured slf4j + logback

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,19 +1,27 @@
+import com.zzzz.controller.GlobalExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Test {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     public static void main(String[] args) {
-        String e1 = "abc@qq.com";
-        String e2 = "abc@qq@qq.com";
-        String e3 = "abc.@@qq.com";
-        String e4 = "@qq.com";
-        String e5 = "abc@";
-        String e6 = "@";
-        String e7 = "abcqq.com";
-        System.out.println(checkEmailValidity(e1));
-        System.out.println(checkEmailValidity(e2));
-        System.out.println(checkEmailValidity(e3));
-        System.out.println(checkEmailValidity(e4));
-        System.out.println(checkEmailValidity(e5));
-        System.out.println(checkEmailValidity(e6));
-        System.out.println(checkEmailValidity(e7));
+//        String e1 = "abc@qq.com";
+//        String e2 = "abc@qq@qq.com";
+//        String e3 = "abc.@@qq.com";
+//        String e4 = "@qq.com";
+//        String e5 = "abc@";
+//        String e6 = "@";
+//        String e7 = "abcqq.com";
+//        System.out.println(checkEmailValidity(e1));
+//        System.out.println(checkEmailValidity(e2));
+//        System.out.println(checkEmailValidity(e3));
+//        System.out.println(checkEmailValidity(e4));
+//        System.out.println(checkEmailValidity(e5));
+//        System.out.println(checkEmailValidity(e6));
+//        System.out.println(checkEmailValidity(e7));
+        logger.debug("DEBUG!");
+        logger.error("ERROR!");
     }
 
     private static boolean checkEmailValidity(String email) {
--- src/main/java/com/zzzz/controller/GlobalExceptionHandler.java
+++ src/main/java/com/zzzz/controller/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.zzzz.controller;
 
 import com.zzzz.service.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,22 +12,26 @@ import java.sql.SQLException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    // TODO logger undefined
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(value = ServiceException.class)
     public ResponseEntity handleServiceException(ServiceException e) {
+        // Log the exception as DEBUG
+        logger.debug(e.getMessage());
         return ResponseEntity.status(e.getStatus()).body(e.getMessage());
     }
 
     @ExceptionHandler(value = SQLException.class)
-    public ResponseEntity handleSqlException() {
-        // TODO log the error
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("内部错误。");
+    public ResponseEntity handleSqlException(SQLException e) {
+        // Log the error
+        logger.error(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("服务内部错误。");
     }
 
     @ExceptionHandler(value = UnsupportedOperationException.class)
     public ResponseEntity<String> handleUnsupportedOperationException() {
-        // TODO log the error
+        // Log the error
+        logger.error("该操作尚未实现。");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("该操作尚未实现。");
     }
 }
