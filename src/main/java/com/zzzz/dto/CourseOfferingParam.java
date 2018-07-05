f5a5571f9c0a45cf3c22f3d053393f81b18e818d
Zenas Chen <czyczk@qq.com>
2018-07-05 21:20:31 +0800

    Service: CourseOfferingService

---
+++ src/main/java/com/zzzz/dto/CourseOfferingParam.java
@@ -0,0 +1,40 @@
+package com.zzzz.dto;
+
+public class CourseOfferingParam {
+    private String courseOfferingId;
+    private String courseId;
+    private String branchId;
+    private String lecturerId;
+
+    public String getCourseOfferingId() {
+        return courseOfferingId;
+    }
+
+    public void setCourseOfferingId(String courseOfferingId) {
+        this.courseOfferingId = courseOfferingId;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(String branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getLecturerId() {
+        return lecturerId;
+    }
+
+    public void setLecturerId(String lecturerId) {
+        this.lecturerId = lecturerId;
+    }
+}
---
+++ src/main/java/com/zzzz/service/CourseOfferingServiceException.java
@@ -0,0 +1,44 @@
+package com.zzzz.service;
+
+import org.springframework.http.HttpStatus;
+
+public class CourseOfferingServiceException extends ServiceException {
+    public enum ExceptionTypeEnum {
+        EMPTY_COURSE_OFFERING_ID("课程课堂编号为空。", HttpStatus.BAD_REQUEST),
+        EMPTY_COURSE_ID("课程编号为空。", HttpStatus.BAD_REQUEST),
+        EMPTY_BRANCH_ID("分部编号为空。", HttpStatus.BAD_REQUEST),
+        EMPTY_LECTURER_ID("讲师编号为空。", HttpStatus.BAD_REQUEST),
+        INVALID_COURSE_OFFERING_ID("课程课堂编号不合法。", HttpStatus.BAD_REQUEST),
+        INVALID_COURSE_ID("课程编号不合法。", HttpStatus.BAD_REQUEST),
+        INVALID_BRANCH_ID("分部编号不合法。", HttpStatus.BAD_REQUEST),
+        INVALID_LECTURER_ID("讲师编号不合法。", HttpStatus.BAD_REQUEST),
+        COURSE_OFFERING_NOT_EXISTING("该课程课堂不存在。", HttpStatus.NOT_FOUND),
+        COURSE_NOT_EXISTING("该课程不存在。", HttpStatus.NOT_FOUND),
+        BRANCH_NOT_EXISTING("该分部不存在。", HttpStatus.NOT_FOUND),
+        LECTURER_NOT_EXISTING("该讲师不存在。", HttpStatus.NOT_FOUND)
+        ;
+
+        private String message;
+        private HttpStatus status;
+
+        public String getMessage() {
+            return message;
+        }
+        public HttpStatus getStatus() {
+            return status;
+        }
+
+        ExceptionTypeEnum(String message, HttpStatus status) {
+            this.message = message;
+            this.status = status;
+        }
+    }
+
+    public CourseOfferingServiceException(String message, HttpStatus status) {
+        super(message, status);
+    }
+
+    public CourseOfferingServiceException(ExceptionTypeEnum exceptionTypeEnum) {
+        this(exceptionTypeEnum.message, exceptionTypeEnum.status);
+    }
+}
