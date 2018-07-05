73083f9aaf1d14828a1301192f13d1f093c7b73b
Zenas Chen <czyczk@qq.com>
2018-07-05 10:44:14 +0800

    Controller: AdministratorController

--- src/main/java/com/zzzz/controller/AdministratorController.java
+++ src/main/java/com/zzzz/controller/AdministratorController.java
@@ -1,13 +1,15 @@
 package com.zzzz.controller;
 
+import com.zzzz.dto.AdministratorParam;
+import com.zzzz.po.Administrator;
 import com.zzzz.service.AdministratorService;
 import com.zzzz.service.AdministratorServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 
 @RestController
@@ -18,24 +20,22 @@ public class AdministratorController {
 
     /**
      * Create a system account.
-     * @param username Username
-     * @param password Password
+     * @param administratorParam username, password
      * @return Success: Administrator ID; Bad request: 400; Internal: 500
      */
-    @RequestMapping(value = "/system", method = RequestMethod.POST)
-    public ResponseEntity<Long> createSystemAccount(String username, String password) throws SQLException, AdministratorServiceException {
-        return createAccount(username, password);
+    @PostMapping(value = "/system")
+    public ResponseEntity<Long> createSystemAccount(@RequestBody AdministratorParam administratorParam) throws SQLException, AdministratorServiceException {
+        return createAccount(administratorParam.getUsername(), administratorParam.getPassword());
     }
 
     /**
      * Create an enterprise account.
-     * @param username Username
-     * @param password Password
+     * @param administratorParam username, password
      * @return Success: Administrator ID; Bad request: 400; Internal: 500
      */
-    @RequestMapping(value = "/enterprise", method = RequestMethod.POST)
-    public ResponseEntity<Long> createEnterpriseAccount(String username, String password) throws SQLException, AdministratorServiceException {
-        return createAccount(username, password);
+    @PostMapping(value = "/enterprise")
+    public ResponseEntity<Long> createEnterpriseAccount(@RequestBody AdministratorParam administratorParam) throws SQLException, AdministratorServiceException {
+        return createAccount(administratorParam.getUsername(), administratorParam.getPassword());
     }
 
     private ResponseEntity<Long> createAccount(String username, String password) throws SQLException, AdministratorServiceException {
@@ -43,5 +43,21 @@ public class AdministratorController {
         // Authentication not implemented yet.
         long administratorId = administratorService.createSystemAccount(username, password);
         return ResponseEntity.ok(administratorId);
+    }
+
+    /**
+     * Log in an administrator account.
+     * @param administratorParam administratorId, password
+     * @return Success: Administrator without its password; Bad request: 400; Not found: 404; Internal: 500
+     */
+    @PostMapping(value = "/login")
+    public ResponseEntity<Administrator> logIn(HttpServletRequest req,
+                                               @RequestBody AdministratorParam administratorParam
+    ) throws SQLException, AdministratorServiceException {
+        Administrator result = administratorService.logIn(administratorParam.getAdministratorId(), administratorParam.getPassword());
+        HttpSession session = req.getSession();
+        session.setAttribute("isLoggedIn", true);
+        session.setAttribute("administratorId", administratorParam.getAdministratorId());
+        return ResponseEntity.ok(result);
     }
 }
---
+++ src/main/java/com/zzzz/dto/AdministratorParam.java
@@ -0,0 +1,31 @@
+package com.zzzz.dto;
+
+public class AdministratorParam {
+    private String administratorId;
+    private String username;
+    private String password;
+
+    public String getAdministratorId() {
+        return administratorId;
+    }
+
+    public void setAdministratorId(String administratorId) {
+        this.administratorId = administratorId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
