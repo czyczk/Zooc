b35e5fbd841ed5cf2cd262986c3b85ca0fee4d72
Zenas Chen <czyczk@qq.com>
2018-07-06 10:45:57 +0800

    Bug fix: Incorrect request mapping in EnterpriseController

--- src/main/java/com/zzzz/controller/EnterpriseController.java
+++ src/main/java/com/zzzz/controller/EnterpriseController.java
@@ -8,13 +8,12 @@ import com.zzzz.service.EnterpriseServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/enterprise")
 public class EnterpriseController {
     @Autowired
     private EnterpriseService enterpriseService;
