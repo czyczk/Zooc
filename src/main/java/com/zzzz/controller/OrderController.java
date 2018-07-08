f5509e81e2d69d70e89712a0e2cde6e6032dc631
Zenas Chen <czyczk@qq.com>
2018-07-08 23:24:14 +0800

    Controller: OrderController. Implemented the functions of Refund functions

--- src/main/java/com/zzzz/controller/OrderController.java
+++ src/main/java/com/zzzz/controller/OrderController.java
@@ -1,8 +1,11 @@
 package com.zzzz.controller;
 
 import com.zzzz.dto.OrderParam;
+import com.zzzz.dto.RefundParam;
 import com.zzzz.service.OrderService;
 import com.zzzz.service.OrderServiceException;
+import com.zzzz.service.RefundService;
+import com.zzzz.service.RefundServiceException;
 import com.zzzz.vo.OrderDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +18,12 @@ import java.util.Date;
 @RequestMapping("/api/v1")
 public class OrderController {
     private final OrderService orderService;
+    private final RefundService refundService;
 
     @Autowired
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, RefundService refundService) {
         this.orderService = orderService;
+        this.refundService = refundService;
     }
 
     /**
@@ -30,6 +35,7 @@ public class OrderController {
     @PutMapping("/course/{id}/order")
     public ResponseEntity<Long> create(@PathVariable("id") String courseId,
                                        @RequestBody OrderParam param) throws SQLException, OrderServiceException {
+        // TODO authentication not implemented yet
         long lastId = orderService.insert(param.getUserId(), courseId, new Date());
         return ResponseEntity.ok(lastId);
     }
@@ -41,6 +47,7 @@ public class OrderController {
      */
     @GetMapping("/order/detail/{id}")
     public ResponseEntity<OrderDetail> getDetailById(@PathVariable("id") String orderId) throws SQLException, OrderServiceException {
+        // TODO authentication not implemented yet
         OrderDetail result = orderService.getVoById(orderId);
         return ResponseEntity.ok(result);
     }
@@ -48,6 +55,7 @@ public class OrderController {
     /**
      * Update an order.
      * Open for the purchaser to cancel the order.
+     * Open for the course owner to approve a refund request.
      * @param orderId Order ID
      * @param param status
      * @return Success: 204; Bad request: 400; Not found: 404; Internal: 500
@@ -55,7 +63,36 @@ public class OrderController {
     @PostMapping("/order/{id}")
     public ResponseEntity update(@PathVariable("id") String orderId,
                                  @RequestBody OrderParam param) throws SQLException, OrderServiceException {
+        // TODO authentication not implemented yet
         orderService.update(orderId, param.getStatus());
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * Apply for a refund. Open for only the purchaser.
+     * @param orderId The ID of the order to be refunded
+     * @param param reason
+     * @return Success: New refund record ID; Bad request: 400; Not found: 404; Internal: 500
+     */
+    @PostMapping("/order/{id}/refund")
+    public ResponseEntity<Long> applyForRefund(@PathVariable("id") String orderId,
+                                               @RequestBody RefundParam param) throws SQLException, RefundServiceException {
+        // TODO authentication not implemented yet
+        long lastId = refundService.insert(orderId, new Date(), param.getReason());
+        return ResponseEntity.ok(lastId);
+    }
+
+    /**
+     * Cancel a refund. Open for only the purchaser.
+     * @param refundId The ID of the refund request to be canceled
+     * @return Success: 204; Bad request: 400; Not found: 404; Internal: 500
+     * @throws SQLException
+     * @throws RefundServiceException
+     */
+    @DeleteMapping("/refund/{id}")
+    public ResponseEntity cancelRefund(@PathVariable("id") String refundId) throws SQLException, RefundServiceException {
+        // TODO authentication not implemented yet
+        refundService.delete(refundId);
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/zzzz/dto/RefundParam.java
@@ -0,0 +1,40 @@
+package com.zzzz.dto;
+
+public class RefundParam {
+    private String refundId;
+    private String orderId;
+    private String time;
+    private String reason;
+
+    public String getRefundId() {
+        return refundId;
+    }
+
+    public void setRefundId(String refundId) {
+        this.refundId = refundId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
