16719c79eb1e2fd08023c6a3d0fffaffb5f31ba3
Zenas Chen <czyczk@qq.com>
2018-07-27 14:02:47 +0800

    Handle RedisConnectionFailureException

--- src/main/java/com/zzzz/controller/GlobalExceptionHandler.java
+++ src/main/java/com/zzzz/controller/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.zzzz.controller;
 import com.zzzz.service.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,6 +27,13 @@ public class GlobalExceptionHandler {
         // Log the error
         logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("服务内部错误。");
+    }
+
+    @ExceptionHandler(value = RedisConnectionFailureException.class)
+    public ResponseEntity handleRedisConnectionFailureException(RedisConnectionFailureException e) {
+        // Log the error
+        logger.error(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("无法连接至 Redis 服务器，请检查网络情况。");
     }
 
     @ExceptionHandler(value = UnsupportedOperationException.class)
